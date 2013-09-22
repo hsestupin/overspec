c22716a7290c267f6759d70cc0c59f1f37b35f5c
Stupin Sergey <stupin.sergey@gmail.com>
2013-09-23 02:03:38 +0600

    rename to-contain arg

--- src/overspec/matchers.clj
+++ src/overspec/matchers.clj
@@ -10,8 +10,8 @@
 (defn to-be-falsy [actual]
   (false? actual))
 
-(defn to-contain [key actual]
-  (contains? actual key))
+(defn to-contain [key coll]
+  (contains? coll key))
 
 (defn to-be-nil [actual]
   (nil? actual))
